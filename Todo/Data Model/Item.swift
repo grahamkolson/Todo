8fb77b65c7cbf12d616f6560cd33c62f02271815
grahamkolson <grahamkolson@gmail.com>
2018-11-19 22:05:14 -0800

    convert to MVC design pattern

---
+++ Todo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todo
+//
+//  Created by Graham K Olson on 2018-11-19.
+//  Copyright © 2018 Graham K Olson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
